a2ab3223a8e87d2e4668cda8af8d44b6931ccaf3
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2022-07-23 22:34:28 -0700

    Added image tiling

--- ImageConverter.java
+++ ImageConverter.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -11,20 +12,27 @@ public class ImageConverter {
 
     public static void main(String[] args) {
         
+        String usageStr = "Usage: [-help] [-m] [-merge] [-s] [-split] [-t] [-tile] [<args>]";
+
         if (args.length < 1) {
-            System.out.println("Usage: [-help] [-m] [-merge] [-s] [-split] [<args>]");
+            System.out.println(usageStr);
             System.exit(0);
         }
 
         else if (args[0].contains("-help")) {
-            System.out.println("Usage: [-help] [-m] [-merge] [-s] [-split] [<args>]\n");
+            System.out.println(usageStr + "\n");
             System.out.println("Merge: [-m] or [-merge] -- merge two images");
             System.out.println("Args: <image 1> <image 2> <mode>");
             System.out.println("Modes:");
             System.out.println("\t0: keep brightest");
             System.out.println("\n");
-            System.out.println("Split: [-s] or [-split]");
+            System.out.println("Split: [-s] or [-split] -- split an image into subimages");
             System.out.println("Args: <image> <slice width> <slice height>");
+            System.out.println("\n");
+            System.out.println("Tile: [-t] or [-tile] -- combine a set of images into a tilemap");
+            System.out.println("Modes:");
+            System.out.println("\t[--d] or [--directory] <out image>: use all images in a directory");
+            System.out.println("\t<image 1> <image 2> ... <out image> : use all image files listed");
             System.exit(0);
         }
 
@@ -35,6 +43,9 @@ public class ImageConverter {
         }
         else if (command.equals("-s") || command.equals("-split")) {
             splitImage(args);
+        }
+        else if (command.equals("-t") || command.equals("-tile")) {
+            tileImages(args);
         }
 
     }
@@ -250,4 +261,198 @@ public class ImageConverter {
         System.out.println("Success!");
 
     }
+
+    private static void tileImages(String[] args) {
+
+        // example args 1: -t -d folder/subfolder out.png
+        // example args 2: -t img1.png img2.png out.png
+
+        // validate args (output file must not exist)
+        String outputTarget = args[args.length - 1];
+        File outputFile = Paths.get(outputTarget).toFile();
+        if (outputFile.exists()) {
+            System.err.println("ERROR: Target file " + outputFile.getName() + " ");
+            System.exit(-1);
+        }
+
+        // get list of image files
+        File[] filesToLoad;
+
+        // Supported image formats
+        String exts = ".png|.jpg";
+
+        if (args[1].equals("--d") || args[1].equals("--directory")) {
+            
+            // get the target directory
+            String dirPath = args[2];
+            File directory = Paths.get(dirPath).toFile();
+
+            // handle errors getting the directory
+            if (!directory.exists()) {
+                System.err.println("ERROR: Target directory " + directory.getName() + " does not exist!");
+                System.exit(-1);
+            }
+            else if (!directory.isDirectory()) {
+                System.err.println("ERROR: Target file " + directory.getName() + " is not a directory!");
+                System.exit(-1);
+            }
+
+            // retrieve all files
+            filesToLoad = directory.listFiles();
+
+            // check if all the files are images -- if not, will need to rebuild the array
+            boolean allSupportedImages = true;
+            int count = 0;
+            for (File file : filesToLoad) {
+                
+                String name = file.getName();
+                String ext = name.substring(name.lastIndexOf('.'));
+
+                if (!exts.contains(ext)) {
+                    allSupportedImages = false;
+                }
+                else count++;
+
+            }
+            
+            // if not all files are supported image files, handle it
+            if (!allSupportedImages) {
+
+                // if no files in the directory are supported image files, warn the user and exit
+                if (count == 0) {
+                    System.err.println("ERROR: No supported image files were found in the target directory.");
+                    System.exit(-1);
+                }
+
+                // if some, but not all, files are supported image files, warn the user and continue
+                System.err.println("WARNING: Not all files in the target directory are supported image types. Only supported images will be included in the output.");
+
+                // rebuild the array
+                File[] rebuilt = new File[count];
+
+                int index = 0;
+                for (File file : filesToLoad) {
+
+                    String name = file.getName();
+                    String ext = name.substring(name.lastIndexOf('.'));
+
+                    if (exts.contains(ext)) {
+                        rebuilt[index++] = file;
+                    }
+
+                }        
+
+            }
+
+        }
+        else {
+
+            // load files from the args list
+            filesToLoad = new File[args.length - 2]; // ignore the command and the output file
+
+            // verify all files
+            for (int i = 0; i < filesToLoad.length; i++) {
+                
+                File file = Paths.get(args[i + 1]).toFile();
+                String name = file.getName();
+
+                // each file must exist
+                if (!file.exists()) {
+                    System.err.println("ERROR: File " + name + " does not exist!");
+                    System.exit(-1);
+                }
+
+                // each file must be a supported image type
+                String ext = name.substring(name.lastIndexOf('.'));
+
+                if (!exts.contains(ext)) {
+                    System.err.println("ERROR: File " + name + " is not a supported image file.");
+                    System.exit(-1);
+                }
+
+                // if none of those failed, add the file to the list
+                filesToLoad[i] = file;
+
+            }
+
+        }
+
+        // load all images, track the largest dimensions
+        BufferedImage[] images = new BufferedImage[filesToLoad.length];
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        for (int i = 0; i < images.length; i++) {
+
+            try {
+                BufferedImage image = ImageIO.read(filesToLoad[i]);
+                images[i] = image;
+                if (image.getWidth() > maxWidth) maxWidth = image.getWidth();
+                if (image.getHeight() > maxHeight) maxHeight = image.getHeight();
+            }
+            catch (IOException e) {
+                System.err.println("ERROR: Could not load image file "  + filesToLoad[i].getName() + " -- " + e.getMessage());
+                System.exit(-1);
+            }
+
+        }
+
+        // determine which dimension is larger to form the tile squares
+        int square = Math.max(maxWidth, maxHeight);
+
+        // compute the smallest perfect square that fits all images
+        int numImages = images.length;
+        int numRows;
+
+        // 1. if it's already a perfect square, no change is necessary
+        int sqrt = (int) Math.sqrt(numImages);
+        if ((int) Math.pow(sqrt, 2) == numImages) numRows = sqrt;
+
+        // if it's not, find the next largest perfect square
+        else numRows = (int) Math.ceil(Math.sqrt(numImages));
+
+        // Announce computed size
+        System.out.println("Tiling images -- grid size: " + numRows + ", cell size: " + square);
+
+        // merge all images into a tilemap
+        BufferedImage result = new BufferedImage(square * numRows, square * numRows, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = result.createGraphics();
+
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numRows; c++) {
+
+                // make sure not to overrun the image array size
+                int imageIndex = r * numRows + c;
+                if (imageIndex >= images.length) break;
+
+                // fetch the image and its dimensions
+                BufferedImage image = images[r * numRows + c];
+                int width = image.getWidth();
+                int height = image.getHeight();
+
+                // find the center of this square
+                int centerX = c * square + square / 2;
+                int centerY = r * square + square / 2;
+
+                // center the image in the square
+                int x = centerX - width / 2;
+                int y = centerY - height / 2;
+
+                // draw the image at the calculated position
+                g.drawImage(image, null, x, y);
+
+            }
+        }
+
+        // save the result
+        try {
+            ImageIO.write(result, "png", outputFile);
+        }
+        catch (IOException e) {
+            System.err.println("ERROR: Could not save result -- " + e.getMessage());
+            System.exit(-1);
+        }
+
+    }
+
 }
